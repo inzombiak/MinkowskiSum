fecbb0f0295679de79825acd0d4b91012461bd18
inzombiak <zgarapetian@gmail.com>
2017-02-28 20:50:35 +0400

    May this be the last time I hear the word "covolution"
    Please don't find any bugs with the convolution.
    Please....p...p..please.

--- MinkowskiSum/Minkowski.cpp
+++ MinkowskiSum/Minkowski.cpp
@@ -30,7 +30,7 @@ std::vector<sf::Vector2f> Minkowski::MinkoswkiSum(const std::vector<sf::Vector2f
 
 	//Perform 1 iteration and if both shapes are convex, we have the M-sum
 	//result = ReducedConvolution(verticesA, aReflexIndices, verticesB, bReflexIndices);
-	result = ReducedConvolution_CGAL(aReflexIndices, bReflexIndices);
+	result = ReducedConvolution(aReflexIndices, bReflexIndices);
 	if (aReflexCount == 0 || bReflexCount == 0)
 		return result;
 	else
@@ -44,8 +44,6 @@ std::vector<sf::Vector2f> Minkowski::MinkoswkiSum(const std::vector<sf::Vector2f
 //Returns all edges(i, i+1) of the reduced convolution
 std::vector<sf::Vector2f> Minkowski::ReducedConvolution(const std::vector<int>& aReflexIndices, const std::vector<int>& bReflexIndices)
 { 
-	//DEBUG
-	int MAX_LOOP = 100;
 	std::vector<sf::Vector2f> result;
 
 	unsigned int AVertexCount = m_aVertices.size();
@@ -64,58 +62,73 @@ std::vector<sf::Vector2f> Minkowski::ReducedConvolution(const std::vector<int>& 
 	sf::Vector2f s, t;
 
 	VisitedEdge currentEdge;
+	VisitedEdge startEdge;
 	bool iIsReflex = false, jIsReflex = false;
+	bool edgeFoundI, edgeFoundJ;
 	//Itereate
 	for (unsigned int i0 = 0; i0 < AVertexCount; ++i0)
 	{
-		i = i0;
-		j = 0;
-		s = m_aVertices[i] + m_bVertices[j];
-		MAX_LOOP = 30;
-		do
+		printf("\n \n ******** \n \n");
+		for (unsigned int j0 = 0; j0 < BVertexCount; ++j0)
 		{
-			MAX_LOOP--;
-			currentEdge.first = i;
-			currentEdge.second = j;
-			printf("i: %i, j: %i \n", i, j);
-			if (m_visited.count(currentEdge))
-				continue;
-
-			next_i = (i + 1) % AVertexCount;
-			next_j = (j + 1) % BVertexCount;
-			prev_i = sfmath::Mod(i - 1, AVertexCount);
-			prev_j = sfmath::Mod(j - 1, BVertexCount);
-
-			iIsReflex = (std::find(aReflexIndices.begin(), aReflexIndices.end(), i) != aReflexIndices.end());
-			jIsReflex = (std::find(bReflexIndices.begin(), bReflexIndices.end(), j) != bReflexIndices.end());
-
-			includeP = IsBetweenCCW(m_aDirections[i], m_bDirections[prev_j], m_bDirections[j]);
-			includeQ = IsBetweenCCW(m_bDirections[j], m_aDirections[prev_i], m_aDirections[i]);
-			if (includeP && !jIsReflex)
+			i = i0;
+			j = j0;
+			s = m_aVertices[i] + m_bVertices[j];
+			printf("\n \n ------- \n \n");
+			do
 			{
-				//m_visited.insert(VisitedEdge(next_i, j));
-				t = m_aVertices[next_i] + m_bVertices[j];
-				result.push_back(s);
-				result.push_back(t);
-				s = t;
+				currentEdge.first = i;
+				currentEdge.second = j;
+				edgeFoundI = false;
+				edgeFoundJ = false;
 
-				i = next_i;
-			}
-			
-			if (includeQ && !iIsReflex)
-			{
-				//m_visited.insert(VisitedEdge(i, next_j));
-				t = m_aVertices[i] + m_bVertices[next_j];
-				result.push_back(s);
-				result.push_back(t);
-				s = t;
+				if (m_visited.count(currentEdge) > 0)
+				{
+					break;
+				}
 
-				j = next_j;
-			}
-		} while (MAX_LOOP > 0 && (i != i0 || j != 0)); // Repeat until we're back where we started
+				printf("i: %i, j: %i \n", i, j);
+				m_visited.insert(currentEdge);
+
+				next_i = (i + 1) % AVertexCount;
+				next_j = (j + 1) % BVertexCount;
+				prev_i = sfmath::Mod(i - 1, AVertexCount);
+				prev_j = sfmath::Mod(j - 1, BVertexCount);
+
+				iIsReflex = (std::find(aReflexIndices.begin(), aReflexIndices.end(), i) != aReflexIndices.end());
+				jIsReflex = (std::find(bReflexIndices.begin(), bReflexIndices.end(), j) != bReflexIndices.end());
+
+				includeP = IsBetweenCCW(m_aDirections[i], m_bDirections[prev_j], m_bDirections[j]);
+				includeQ = IsBetweenCCW(m_bDirections[j], m_aDirections[prev_i], m_aDirections[i]);
+
+				if (includeP)
+				{
+					i = next_i;
+					t = m_aVertices[next_i] + m_bVertices[j];
+					if (!jIsReflex)
+					{
+						result.push_back(s);
+						result.push_back(t);
+					}
+					s = t;
+				}
+				if (includeQ)
+				{
+					j = next_j;
+					t = m_aVertices[i] + m_bVertices[next_j];
+					if (!iIsReflex)
+					{
+						result.push_back(s);
+						result.push_back(t);
+					}
+					s = t;
+				}
+
+			} while (j != j0 && i != i0);// Repeat until we're back where we started
+
+		}
 	}
-	
-	printf("Convolution Size: %i MAX_LOOP: %i \n", result.size(), MAX_LOOP);
+	printf("Convolution Size: %i MAX_LOOP: %i \n", result.size());
 	return result;
 }
 
@@ -139,7 +152,7 @@ bool Minkowski::IsBetweenCCW(sf::Vector2f eVec, sf::Vector2f e1Vec,sf::Vector2f 
 	float crossE1E, crossE1E2, crossE2E, crossE2E1;
 	crossE1E = sfmath::Cross(e1Vec, eVec);
 	crossE2E = sfmath::Cross(e2Vec, eVec);
-
+	
 	crossE1E2 = sfmath::Cross(e1Vec, e2Vec);
 	crossE2E1 = sfmath::Cross(e2Vec, e1Vec);
 
@@ -161,9 +174,9 @@ bool Minkowski::IsBetweenCCW(sf::Vector2f eVec, sf::Vector2f e1Vec,sf::Vector2f 
 	{
 		//If E is CCW from E1, it is valid otherwise, check if it is inbetween
 		if (crossE1E < 0)
-			return false;
+			return true;
 		else
-			return ((crossE1E * crossE1E2) >= 0 && (crossE2E * crossE2E1) >= 0);
+			return !((crossE1E * crossE1E2) >= 0 && (crossE2E * crossE2E1) >= 0);
 	}
 
 	return false;
@@ -181,31 +194,7 @@ int Minkowski::MinIndex(const std::vector<sf::Vector2f>& vertices)
 	return min;
 }
 
-/*
-	CGAL CODE FOR REFERENCE: https://github.com/CGAL/cgal/blob/master/Minkowski_sum_2/include/CGAL/Minkowski_sum_2/Minkowski_sum_by_reduced_convolution_2.h\
-	I haven't used CGAL, but the code provided in the link above did not work for my polygons without one simple change. 
-	This issue may be my fault, but I'm going to explain what I found anyway.
-	I found that if either polygon was concave, then the algorithm would fail depending on what order the vertices were placed.
-	Testing on a simple 4 vertex concave polygon(arrow-shaped), I found it would fail everytime unless the reflex vertex was the first vertex.
-	I've attempted to draw a dengenerate case. If we move the first vertex to the relfex angle, the algorithm works as is.
-	                   
-					   1
-	                  * ^
-	0 < * * 3		 * B *
-	*       ^		*     * 
-	*   A   *	   *   3   * 
-	v       *     v  ^   v  *
-	1 * * > 2    2 *       * 0 
-
-	An interesting thing to note is that when the code reaches A0, B0, it does not find any valid edges, controdicting what is written in the paper.
-	The specific issue was that A1 + B3 -> A1 + B0 was never added to the final convolution.
-
-	The fix is simple: move state_queue.push(State(new_i1, new_i2)) outside of the "if".
-	My reasoning for this is that for a concave polygon a loop isn't closed, so we won't traverse all the vertices if we only move forwards(or backwards).
-	When I did it by hand I found that the algorithm would ONLY consider the B3 when traversing the edges of A and since it is a reflex vertex it would not be valid, 
-	and the state "A1 B3" would never be added to the queue. By adding it to the queue, B3 ends up being considered with A0->A1, completing the convolution.
-
-*/
+//CGAL CODE USED FOR REFERENCE
 #include <queue>
 typedef std::pair<int, int>                           State;
 
@@ -270,19 +259,9 @@ std::vector<sf::Vector2f> Minkowski::ReducedConvolution_CGAL(const std::vector<i
 				belongs_to_convolution = IsBetweenCCW(m_aDirections[i1], m_bDirections[prev_i2], m_bDirections[i2]);
 			else
 				belongs_to_convolution = IsBetweenCCW(m_bDirections[i2], m_aDirections[prev_i1], m_aDirections[i1]);
-			//New location
-			state_queue.push(State(new_i1, new_i2));
-			if (new_i1 == 1 && new_i2 == 3)
-			{
-				//This is the point that would be the cause of the issue for my test case
-				//It would be considered when traversing A, and since it is reflex it wouldn't be added to the list
-				//But it WOULD'NT be considered as a part of the edge 3->0, thus leading to an incomplete convolution
-			}
 			if (belongs_to_convolution)
 			{
-
-				//Old location
-				//state_queue.push(State(new_i1, new_i2));
+				state_queue.push(State(new_i1, new_i2));
 				// Only edges added to convex vertices can be on the M-sum's boundary.
 				// This filter only leaves the *reduced* convolution.
 				bool concave;
